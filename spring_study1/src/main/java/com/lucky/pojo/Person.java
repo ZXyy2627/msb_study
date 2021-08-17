91f3aa82fc0fadb4b4193f9ec179c12a65321f55
ZXyy2627 <1375423395@qq.com>
2021-08-17 22:15:36 +0800

    SpringIOC初体验，解耦

---
+++ spring_study1/src/main/java/com/lucky/pojo/Person.java
@@ -0,0 +1,21 @@
+package com.lucky.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author: lucky_x
+ * @Date: 2021/8/17 - 08 - 17 - 22:02
+ * @Description: com.lucky.pojo
+ * @VERSION: 1.0
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String gender;
+}
---
+++ spring_study1/src/test/java/MyTest.java
@@ -0,0 +1,26 @@
+import com.lucky.pojo.Person;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @Author: lucky_x
+ * @Date: 2021/8/17 - 08 - 17 - 22:10
+ * @Description: PACKAGE_NAME
+ * @VERSION: 1.0
+ */
+public class MyTest {
+    static ApplicationContext context;
+
+    static {
+        context = new ClassPathXmlApplicationContext("ioc.xml");
+    }
+    /**
+     * 测试IOC容器配置bean，并使用property标签初始化对象
+     */
+    @Test
+    public void test1() {
+        Person person1 = context.getBean("person1", Person.class);
+        System.out.println(person1);
+    }
+}
