cdd6bdb4e6e1bfe9f7ca8db8587651c323508b53
ZXyy2627 <1375423395@qq.com>
2021-08-19 15:43:50 +0800

    使用注解注册bean

---
+++ spring_annotation_study/src/test/java/MyTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * @Author: lucky_x
+ * @Date: 2021/8/19 - 08 - 19 - 13:51
+ * @Description: PACKAGE_NAME
+ * @VERSION: 1.0
+ */
+public class MyTest {
+
+    @Test
+    public void test1() {
+
+    }
+}
