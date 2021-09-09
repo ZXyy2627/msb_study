ba959998259cfee1d84c80ab34ca31de656a0b7a
ZXyy2627 <1375423395@qq.com>
2021-09-09 21:07:30 +0800

    面试题，对读方法不加锁，对写方法加锁会产生脏读

--- juc_test/src/main/java/com/lucky/jucTest/c_008/Account.java
+++ juc_test/src/main/java/com/lucky/jucTest/c_008/Account.java
@@ -29,7 +29,7 @@ public class Account {
 
     public static void main(String[] args) {
         Account a = new Account();
-        new Thread(() -> a.set("zhangsan",100.0)).start();
+        new Thread(() -> a.set("zhangSan",100.0)).start();
 
         try {
             Thread.sleep(1);
