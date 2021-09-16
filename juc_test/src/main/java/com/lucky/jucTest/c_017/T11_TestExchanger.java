2d0e0cba46e05822446917d8c147bf67a961da23
ZXyy2627 <1375423395@qq.com>
2021-09-16 21:04:47 +0800

    交换变量值

---
+++ juc_test/src/main/java/com/lucky/jucTest/c_017/T11_TestExchanger.java
@@ -0,0 +1,36 @@
+package com.lucky.jucTest.c_017;
+
+import java.util.concurrent.Exchanger;
+
+/**
+ * @Author: lucky_x
+ * @Date: 2021/9/16 - 09 - 16 - 20:57
+ * @Description: com.lucky.jucTest.c_017
+ * @VERSION: 1.0
+ */
+public class T11_TestExchanger {
+
+    static Exchanger<String> exchanger = new Exchanger<>();
+    public static void main(String[] args) {
+        new Thread( () -> {
+            String s = "s1111";
+            try {
+                s = exchanger.exchange(s);
+                System.out.println(Thread.currentThread().getName()+": s = " + s);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        },"t1").start();
+
+        new Thread( () -> {
+            String s = "s2222";
+            try {
+                s = exchanger.exchange(s);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + ": s = " + s);
+        },"t2").start();
+
+    }
+}
