580b1f4181f4a974d50386dc9d5be6fb713a5722
ZXyy2627 <1375423395@qq.com>
2021-09-16 16:32:03 +0800

    CountDownLatch倒数门闩

---
+++ juc_test/src/main/java/com/lucky/jucTest/c_017/T06_TestCountDownLatch.java
@@ -0,0 +1,71 @@
+package com.lucky.jucTest.c_017;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @Author: lucky_x
+ * @Date: 2021/9/16 - 09 - 16 - 15:51
+ * @Description: com.lucky.jucTest.c_017
+ * @VERSION: 1.0
+ *
+ * 对比join和CountDownLatch的效果
+ *
+ * 什么是CountDownLatch?作用是什么？
+ */
+public class T06_TestCountDownLatch {
+
+    public static void main(String[] args) {
+        //usingCountDownLatch();
+        usingJoin();
+    }
+
+    private static void usingCountDownLatch() {
+        Thread[] threads = new Thread[100];
+        CountDownLatch latch = new CountDownLatch(threads.length);
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(() -> {
+                int result = 0;
+                for (int j = 0; j < 10000; j++) result+=j;
+                latch.countDown();
+            });
+        }
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].start();
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("end latch");
+    }
+
+
+    private static void usingJoin() {
+        Thread[] threads = new Thread[100];
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(() -> {
+               int result = 0;
+               for (int j = 0; j < 10000; j++) result+=j;
+            });
+        }
+
+        for (int i = 0; i < threads.length; i++) {
+            threads[i].start();
+        }
+
+        for (int i = 0; i < threads.length; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("end join");
+    }
+}
