60cc055d1c749b4366e65cc223c8ef7ad3725bdc
ZXyy2627 <1375423395@qq.com>
2021-09-16 16:58:05 +0800

    循环栅栏CyclicBarrier

--- juc_test/src/main/java/com/lucky/jucTest/c_017/T07_TestCyclicBarrier.java
+++ juc_test/src/main/java/com/lucky/jucTest/c_017/T07_TestCyclicBarrier.java
@@ -13,7 +13,7 @@ import java.util.concurrent.CyclicBarrier;
  */
 public class T07_TestCyclicBarrier {
     public static void main(String[] args) {
-        CyclicBarrier barrier = new CyclicBarrier(20,() -> System.out.println("满人，发车"));
+        CyclicBarrier barrier = new CyclicBarrier(20,()->System.out.println("满人，发车"));
 
         for (int i = 0; i < 100; i++) {
             new Thread(() -> {
